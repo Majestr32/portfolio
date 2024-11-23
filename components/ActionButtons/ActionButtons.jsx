f316d7dccc236d7bb47bb12794a97216a6748ce6
PandasCodi <61013205+Majestr32@users.noreply.github.com>
2024-11-23 16:08:57 +0200

    button link

--- components/ActionButtons/ActionButtons.jsx
+++ components/ActionButtons/ActionButtons.jsx
@@ -13,12 +13,14 @@ export const ActionButtons = () => {
         </button>
       </Link>
 
-      <button
-        type="button"
-        className="text-white text-xl hover:bg-gray-900 border border-white font-semibold font-poppins rounded-full text-sm md:px-6 md:py-3.5 px-3 py-2.5 text-center me-2 mb-2"
-      >
-        Download CV
-      </button>
+      <Link href="https://cv.djinni.co/f3/b39008ac18572471360e703bb81f5d/cv_eng.pdf">
+        <button
+          type="button"
+          className="text-white text-xl hover:bg-gray-900 border border-white font-semibold font-poppins rounded-full text-sm md:px-6 md:py-3.5 px-3 py-2.5 text-center me-2 mb-2"
+        >
+          Download CV
+        </button>
+      </Link>
     </div>
   );
 };
